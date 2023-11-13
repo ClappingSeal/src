7de49a4399af5ddcc3d0ab4c3d9e4f8c5822f051
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2023-11-13 22:01:01 +0900

    Add files via upload

---
+++ ppo_package/ppo_package/ppo_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from ppo_package.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ td3_package/td3_package/td3_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from td3_package.')
+
+
+if __name__ == '__main__':
+    main()
