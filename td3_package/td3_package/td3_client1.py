9d9df1a37f9f76e0488d9ade804021a4be80654c
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2023-11-13 22:11:33 +0900

    Add files via upload

---
+++ td3_package/td3_package/td3_client1.py
@@ -0,0 +1,84 @@
+import rclpy
+from rclpy.node import Node
+from msgs.srv import TD3
+from msgs.srv import GOTO
+from msgs.srv import LAND
+import argparse
+import math
+import time
+
+
+class TD3ClientNode(Node):
+    def __init__(self, goal_x, goal_y):
+        super().__init__('td3_client1')
+        self.cli_td3 = self.create_client(TD3, 'td31')
+        while not self.cli_td3.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Waiting for TD3 service to become available...')
+
+        self.cli_goto = self.create_client(GOTO, 'goto1')
+        while not self.cli_goto.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Waiting for GOTO service to become available...')
+
+        self.cli_land = self.create_client(LAND, 'land1')
+        while not self.cli_land.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Waiting for LAND service to become available...')
+
+        self.req = TD3.Request()
+        self.send_request([goal_x, goal_y])
+
+    def send_request(self, goal):
+        self.req.goal = goal
+        future = self.cli_td3.call_async(self.req)
+        rclpy.spin_until_future_complete(self, future)
+
+        if future.result() is not None:
+            path = list(future.result().path)
+            print(path)
+
+            goto_request = GOTO.Request()
+            goto_request.x = float(path[0])
+            goto_request.y = float(path[1])
+            goto_request.z = float(3) # 높이 세팅 값
+            future_goto = self.cli_goto.call_async(goto_request)
+            rclpy.spin_until_future_complete(self, future_goto)
+
+            if future_goto.result() is not None:
+                self.get_logger().info(f'Result of GOTO: {future_goto.result().message}')
+
+                # Check distance to target
+                distance = math.sqrt((goal[0] - path[0]) ** 2 + (goal[1] - path[1]) ** 2)
+                threshold_distance = 0.5  # 골 세팅값
+
+                if distance <= threshold_distance:
+                    land_request = LAND.Request()
+                    future_land = self.cli_land.call_async(land_request)
+                    rclpy.spin_until_future_complete(self, future_land)
+
+                    if future_land.result() is not None:
+                        self.get_logger().info(f'Result of LAND: {future_land.result().message}')
+
+                    while True:
+                        self.get_logger().info('Mission Complete')
+                        time.sleep(5000)
+
+            else:
+                self.get_logger().error('Exception while calling GOTO service')
+
+        else:
+            self.get_logger().error('Exception while calling TD3 service')
+
+
+def main(args=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('goal_x', type=float, help='X coordinate of goal')
+    parser.add_argument('goal_y', type=float, help='Y coordinate of goal')
+    args = parser.parse_args()
+
+    rclpy.init(args=None)
+    client_node = TD3ClientNode(args.goal_x, args.goal_y)
+    client_node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ td3_package/td3_package/td3_node1.py
@@ -0,0 +1,67 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float64MultiArray
+from msgs.srv import TD3
+import numpy as np
+import math
+from stable_baselines3 import TD3
+
+model = TD3.load("TD3.zip")
+
+
+def td3(start, goal, obstacles, velocity=3):
+    start = np.array(start)
+    goal = np.array(goal) - start
+    obstacle0 = obstacles[0] - start
+    obstacle1 = obstacles[1] - start
+    obstacle2 = obstacles[2] - start
+
+    observation = np.concatenate((goal, obstacle0, obstacle1, obstacle2, [0, 0], [0, 0], [0, 0]), axis=0)
+
+    observation = np.array(observation) * 0.04530495
+    action, _ = model.predict(observation)
+
+    return action * velocity
+
+
+class TD3Node1(Node):
+    def __init__(self):
+        super().__init__('td3_node1')
+        self.srv = self.create_service(TD3, 'td31', self.td3_callback)
+        self.subscription = self.create_subscription(Float64MultiArray, 'drone_info', self.listener_callback, 10)
+        self.drone_id = 1.0  # setting value !!!
+        self.current_positions = {}
+        self.start = None
+
+    def listener_callback(self, msg):
+        data = np.array(msg.data)
+        n, x, y, _ = data
+        if n == self.drone_id:
+            self.start = np.array([x, y])
+            self.get_logger().info(f'Start{self.drone_id}: {self.start}')
+        else:
+            self.current_positions[n] = np.array([x, y])
+            self.get_logger().info(f'Obstacle: {self.current_positions[n]} for drone {n}')
+
+    def td3_callback(self, request, response):
+        obstacles = list(self.current_positions.values())
+        # obstacles.append([3, 5])
+        if self.start is None:
+            self.get_logger().warn('Waiting for drone start position...')
+            return response
+        goal = np.array([request.goal[0], request.goal[1]])
+        next_position = td3(self.start, goal, obstacles)
+        response.path = list(np.array(next_position).flatten())
+        return response
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    td3_node1 = TD3Node1()
+    rclpy.spin(td3_node1)
+    td3_node1.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
