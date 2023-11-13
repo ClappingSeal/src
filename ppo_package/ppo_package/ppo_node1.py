be3a7444f38122a8b684794ff5d744b375d9ff64
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2023-11-13 22:04:51 +0900

    Add files via upload

---
+++ ppo_package/ppo_package/ppo_node1.py
@@ -0,0 +1,86 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float64MultiArray
+from msgs.srv import PPO
+import numpy as np
+import math
+from stable_baselines3 import PPO
+
+model = PPO.load("PPO.zip")
+
+
+def ppo(start, goal, obstacles, velocity=3):
+    def change_action(num):
+        sq = math.sqrt(2) / 2
+        if num == 0:
+            return np.array([0, velocity])
+        elif num == 1:
+            return np.array([velocity * sq, velocity * sq])
+        elif num == 2:
+            return np.array([velocity, 0])
+        elif num == 3:
+            return np.array([velocity * sq, -velocity * sq])
+        elif num == 4:
+            return np.array([0, -velocity])
+        elif num == 5:
+            return np.array([-velocity * sq, -velocity * sq])
+        elif num == 6:
+            return np.array([-velocity, 0])
+        elif num == 7:
+            return np.array([-velocity * sq, velocity * sq])
+
+    start = np.array(start)
+    goal = np.array(goal) - start
+    obstacle0 = obstacles[0] - start
+    obstacle1 = obstacles[1] - start
+    obstacle2 = obstacles[2] - start
+
+    observation = np.concatenate((goal, obstacle0, obstacle1, obstacle2, [0, 0], [0, 0], [0, 0]), axis=0)
+
+    observation = np.array(observation) * 0.4530495 / 30
+    action, _ = model.predict(observation)
+
+    return start + change_action(action)
+
+
+class PPONode1(Node):
+    def __init__(self):
+        super().__init__('ppo_node1')
+        self.srv = self.create_service(PPO, 'ppo1', self.ppo_callback)
+        self.subscription = self.create_subscription(Float64MultiArray, 'drone_info', self.listener_callback, 10)
+        self.drone_id = 1.0  # setting value !!!
+        self.current_positions = {}
+        self.start = None
+
+    def listener_callback(self, msg):
+        data = np.array(msg.data)
+        n, x, y, _ = data
+        if n == self.drone_id:
+            self.start = np.array([x, y])
+            self.get_logger().info(f'Start{self.drone_id}: {self.start}')
+        else:
+            self.current_positions[n] = np.array([x, y])
+            self.get_logger().info(f'Obstacle: {self.current_positions[n]} for drone {n}')
+
+    def ppo_callback(self, request, response):
+        obstacles = list(self.current_positions.values())
+        # obstacles.append([3, 5])
+        if self.start is None:
+            self.get_logger().warn('Waiting for drone start position...')
+            return response
+        goal = np.array([request.goal[0], request.goal[1]])
+        next_position = ppo(self.start, goal, obstacles)
+        response.path = list(np.array(next_position).flatten())
+        return response
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    ppo_node1 = PPONode1()
+    rclpy.spin(ppo_node1)
+    ppo_node1.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
