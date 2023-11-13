c59738d43915476a4be9e9db926534b5b5e0fe0a
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2023-11-13 23:55:54 +0900

    Update td3_node2.py

--- td3_package/td3_package/td3_node2.py
+++ td3_package/td3_package/td3_node2.py
@@ -55,7 +55,7 @@ class TD3Node2(Node):
             return response
         goal = np.array([request.goal[0], request.goal[1]])
         next_position = td3(self.start, goal, obstacles)
-        response.path = next_position
+        response.path = list(np.array(next_position).flatten())
         return response
 
 
