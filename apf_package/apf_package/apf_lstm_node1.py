d74b6a182cca2cfe47fa19549a40c7086236c32a
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2023-12-07 18:31:33 +0900

    Update apf_lstm_node1.py

--- apf_package/apf_package/apf_lstm_node1.py
+++ apf_package/apf_package/apf_lstm_node1.py
@@ -6,26 +6,33 @@ import numpy as np
 from tensorflow.keras.models import load_model
 
 # TensorFlow와 Keras 버전 확인
-# 라즈베리 파이에서 호환 가능한 버전을 사용하고 있는지 확인하세요
 import tensorflow as tf
 print("TensorFlow version:", tf.__version__)
 import keras
 print("Keras version:", keras.__version__)
-
-# 모델을 로드하기 전에 필요한 설정을 추가할 수 있습니다
-# 예를 들어, 메모리 사용량을 제한하거나, 모델 최적화를 위한 설정을 할 수 있습니다
-# 이 부분은 라즈베리 파이의 하드웨어에 따라 조정해야 할 수 있습니다
 
 # 모델 로드
 try:
     model_LSTM = load_model('lstm_drone_positions_model.keras')
 except Exception as e:
     print("모델 로드 중 오류 발생:", e)
-    # 여기에서 추가적인 오류 처리 또는 로깅을 할 수 있습니다
 
 def main():
-    # 여기에 실행 로직을 작성하세요
-    pass
+    # ROS2 초기화
+    rclpy.init(args=None)
+
+    # 여기에 노드 생성 및 실행 로직을 작성하세요
+    # 예: node = MyNode()
+
+    try:
+        # ROS2 스피닝 - 노드가 메시지를 받고 처리할 수 있도록 합니다.
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        print('노드 종료')
+    finally:
+        # ROS2 정리
+        node.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == "__main__":
     main()
