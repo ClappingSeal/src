ec8ac7a58ea0c449f6093fa1e66204d9ab8d9b2d
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2023-12-08 02:21:52 +0900

    Update lstm_test.py

--- lstm_test.py
+++ lstm_test.py
@@ -1,25 +1,17 @@
-import pandas as pd
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
 import numpy as np
-import gc
+import tensorflow as tf
 
-# 모델 구조를 정의하는 함수
-def create_model():
-    model = Sequential()
-    model.add(LSTM(20, activation='tanh', input_shape=(3, 2), return_sequences=True))
-    model.add(LSTM(20, activation='tanh'))
-    model.add(Dense(2))
-    return model
+# TensorFlow Lite 모델 로드
+interpreter = tf.lite.Interpreter(model_path='lstm_drone_positions_model.tflite')
+interpreter.allocate_tensors()
 
-
-# 모델을 생성하고 가중치를 로드합니다.
-model_LSTM = create_model()
-model_LSTM.load_weights('lstm_drone_positions_model.keras')
+# 입력 및 출력 텐서 정보 가져오기
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 
-# LSTM 예측 함수
-def LSTM_prediction(array, time_steps=3):
+# LSTM 예측 함수 (TensorFlow Lite 사용)
+def LSTM_prediction_tflite(array, time_steps=3):
     def inverse_min_max_scale(scaled_data, min_val, max_val):
         return scaled_data * (max_val - min_val) + min_val
 
@@ -39,11 +31,13 @@ def LSTM_prediction(array, time_steps=3):
     array = np.array(array, dtype=np.float32)
     X_new, min_val, max_val = preprocess_data(array, time_steps)
 
-    predictions = model_LSTM.predict(X_new, batch_size=1)
-    predictions_rescaled = inverse_min_max_scale(predictions, min_val, max_val)
+    # TensorFlow Lite 모델로 예측
+    interpreter.set_tensor(input_details[0]['index'], X_new)
+    interpreter.invoke()
+    predictions = interpreter.get_tensor(output_details[0]['index'])
 
-    del X_new, predictions
-    gc.collect()
+    # 예측값 역변환
+    predictions_rescaled = inverse_min_max_scale(predictions, min_val, max_val)
 
     return predictions_rescaled
 
@@ -56,4 +50,4 @@ new_data = np.array([
     [0, 0]  # None 대신 0 사용
 ])
 
-print(LSTM_prediction(new_data))
+print(LSTM_prediction_tflite(new_data))
