b761142cb8156b04f0f95febac1d7a75bd3e5883
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2023-12-07 15:59:22 +0900

    Update apf_lstm_node1.py

--- apf_package/apf_package/apf_lstm_node1.py
+++ apf_package/apf_package/apf_lstm_node1.py
@@ -1,11 +1,42 @@
-import rclpy
-from rclpy.node import Node
-from std_msgs.msg import Float64MultiArray
-from msgs.srv import APF
-import numpy as np
+import pandas as pd
 from tensorflow.keras.models import load_model
-from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 
-# 저장된 모델 로드
+# LSTM 모델 로드
 model_LSTM = load_model('lstm_drone_positions_model.keras')
 
+
+# LSTM 예측 함수
+def LSTM_prediction(array, time_steps=3):
+    def inverse_min_max_scale(scaled_data, min_val, max_val):
+        return scaled_data * (max_val - min_val) + min_val
+
+    def preprocess_data(new_data, time_steps=1):
+        def min_max_scale(data):
+            min_val = np.min(data, axis=0)
+            max_val = np.max(data, axis=0)
+            scaled_data = (data - min_val) / (max_val - min_val)
+            return scaled_data, min_val, max_val
+
+        scaled_data, min_val, max_val = min_max_scale(new_data)
+        X_new = []
+        for i in range(len(scaled_data) - time_steps):
+            X_new.append(scaled_data[i:(i + time_steps), :])
+        return np.array(X_new), min_val, max_val
+
+    array = np.nan_to_num(array)
+    X_new, min_val, max_val = preprocess_data(array, time_steps)
+    predictions = model_LSTM.predict(X_new)
+    predictions_rescaled = inverse_min_max_scale(predictions, min_val, max_val)
+    return predictions_rescaled
+
+
+# 예측 실행
+new_data = np.array([
+    [1, 5],
+    [1, 5],
+    [1, 5],
+    [0, 0]  # None 대신 0 사용
+])
+
+print(LSTM_prediction(new_data))
