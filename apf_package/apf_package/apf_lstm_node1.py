33b95b12a20bc2c38a1c56ed377be6437d362e6a
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2023-12-07 16:05:55 +0900

    Update apf_lstm_node1.py

--- apf_package/apf_package/apf_lstm_node1.py
+++ apf_package/apf_package/apf_lstm_node1.py
@@ -4,7 +4,6 @@ from std_msgs.msg import Float64MultiArray
 from msgs.srv import APF
 import numpy as np
 from tensorflow.keras.models import load_model
-from sklearn.preprocessing import MinMaxScaler
 
 # 저장된 모델 로드
 model_LSTM = load_model('lstm_drone_positions_model.keras')
