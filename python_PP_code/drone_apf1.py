371344975196b9557791d2fcc8c2bef2959254f9
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2024-07-29 18:54:15 +0900

    Update drone_apf1.py

--- python_PP_code/drone_apf1.py
+++ python_PP_code/drone_apf1.py
@@ -15,7 +15,7 @@ class PositionSubscriber(Node):
         self.height = 4
         self.limit = 3
         self.goal_threshold = 1
-        self.time = 0.5
+        self.timer = 0.5
         
         self.subscription = self.create_subscription(
             Float64MultiArray,
