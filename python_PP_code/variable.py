21c38bc88aa077288078552c6fc2abda2f5197c6
ClappingSeal <139230370+ClappingSeal@users.noreply.github.com>
2024-07-31 21:22:59 +0900

    Update variable.py

--- python_PP_code/variable.py
+++ python_PP_code/variable.py
@@ -3,5 +3,5 @@ import numpy as np
 class Var:
     def __init__(self):
         self.timer = 1.5
-        self.force_magnitude = 3
-        self.goal_threshold = 1
+        self.force_magnitude = 8
+        self.goal_threshold = 1.5
